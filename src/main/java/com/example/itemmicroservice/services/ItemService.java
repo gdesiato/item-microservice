3bfb3175c41f770b694aed573030084e706907d7
Giuseppe Desiato <giodesiato@gmail.com>
2023-02-27 06:26:41 -0500

    service

--- src/main/java/com/example/itemmicroservice/services/ItemService.java
+++ src/main/java/com/example/itemmicroservice/services/ItemService.java
@@ -1,16 +1,30 @@
 package com.example.itemmicroservice.services;
 
 import com.example.itemmicroservice.models.Item;
+import com.example.itemmicroservice.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
 
     public Object getAllItems() {
         return null;
     }
 
     public Object getItemById(Long id) {
-        return null;
+        Optional<Item> optional;
+        if ((optional = itemRepository.findById(id)).isEmpty()) {
+            return null;
+        } else {
+            return optional.get();
+        }
     }
 
     public Item insertNewItem(Item item) {
